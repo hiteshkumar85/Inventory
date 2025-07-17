c1d6cf238adb98b6b6f2e7732257cbaffe0311e1
hiteshkumar85 <hiteshkumar853391@gmail.com>
2025-07-17 20:09:22 +0530

    h

--- Backend/routes/loginRoute.js
+++ Backend/routes/loginRoute.js
@@ -26,7 +26,7 @@ router.post('/login', async (req, res) => {
 router.post('/saveAdmin', async (req, res) => {
     try {
         const userCount = await User.countDocuments({});
-        res.send(user);
+        res.send(userCount);
         if (userCount === 0) {
             const name = process.env.USERNAME;
             const hashedPassword = await bcrypt.hash(process.env.USERPASSWORD, 10);
