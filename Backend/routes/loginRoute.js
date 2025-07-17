67ab7d286bcc4c4f85d92a52ba21ea11f0ea4214
hiteshkumar85 <hiteshkumar853391@gmail.com>
2025-07-17 19:55:22 +0530

    try catch

--- Backend/routes/loginRoute.js
+++ Backend/routes/loginRoute.js
@@ -8,15 +8,19 @@ dotenv.config();
 
 router.post('/login', async (req, res) => {
     const { name, password } = req.body;
-    const user = await User.findOne({ name });
-    res.send(user);
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json({ message: 'Invalid user details' });
+    try {
+        const user = await User.findOne({ name });
+        res.send(user);
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid user details' });
+        }
+        user.lastLogin = new Date();
+        await user.save();
+        const token = jwt.sign({ id: user._id, name: user.name, role: user.role }, process.env.SECRETKEY, { expiresIn: '1h' });
+        res.json(token);
+    } catch (err) {
+        res.send({ message: err.message });
     }
-    user.lastLogin = new Date();
-    await user.save();
-    const token = jwt.sign({ id: user._id, name: user.name, role: user.role }, process.env.SECRETKEY, { expiresIn: '1h' });
-    res.json(token);
 });
 
 router.post('/saveAdmin', async (req, res) => {
@@ -30,7 +34,7 @@ router.post('/saveAdmin', async (req, res) => {
             res.json(user);
         }
     } catch (err) {
-        res.json({message : err.message});
+        res.json({ message: err.message });
     }
 });
 
